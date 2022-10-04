d507fe3b86991b4799c0a149e08d70cdef81c893
Smit Kukadiya <71758280+smit-kukadiya@users.noreply.github.com>
2022-10-04 14:39:05 +0530

    Messgae related

---
+++ message.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Message</title>
+    </head>
+    <body>
+        <?php
+            $name = $_POST['name'];
+            echo $name;
+        ?>
+    </body>
+</html>
