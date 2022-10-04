f5e69b009572ea4a58d21e2f63ad1dd39bc23053
Smit Kukadiya <71758280+smit-kukadiya@users.noreply.github.com>
2022-10-04 21:17:57 +0530

    adding new file

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Login on Women Walfare</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+    <body>
+        <form action='#' action='post'>
+            Login <input type="text" name='login'> <br/>
+            Password <input type='password' name='pass'> <br/>
+            <input type='submit'>
+        </form>
+    </body>
+</html>
