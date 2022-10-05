69d6b0a2323dbfad7b3821a291a9e76462d3bad3
Smit Kukadiya <71758280+smit-kukadiya@users.noreply.github.com>
2022-10-05 16:17:18 +0530

    Update message.php

--- message.php
+++ message.php
@@ -4,7 +4,14 @@
         <title>Message</title>
     </head>
     <body>
-        <?php
+        <?php 
+            $GithubRawURI= "https://raw.githubusercontent.com/UserName/GitName/FileName.php”; 
+            $ch = curl_init(); 
+            curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+            $data = curl_exec($ch); 
+            curl_close($ch); 
             $name = $_GET['name'];
             echo $name;
         ?>
